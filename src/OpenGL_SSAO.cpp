a261f9db6694291c5ebfcbabde590d618bb7c5cb
Dilin71828 <wangyinhao71828@gmail.com>
2020-08-10 20:10:29 +0800

    add the basic frame

--- src/OpenGL_SSAO.cpp
+++ src/OpenGL_SSAO.cpp
@@ -11,3 +11,135 @@
 #include <myOpenGL/camera.h>
 #include <myOpenGL/shader.h>
 #include <myOpenGL/model.h>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void mouse_callback(GLFWwindow *window, double xPos, double yPos);
+void scroll_callback(GLFWwindow *window, double xOffset, double yOffset);
+void processInput(GLFWwindow *window);
+void renderScene(Shader &shader);
+void renderQuad();
+
+// basic window setting
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
+
+// global time
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+// camera control
+Camera mainCamera(glm::vec3(0.0f, 2.0f, 8.0f));
+float nearPlane = 0.1f;
+float farPlane = 50.0f;
+float lastX = (float)SCREEN_WIDTH / 2.0f;
+float lastY = (float)SCREEN_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// render setting
+const int DEPTH_MAP_WIDTH = 1024;
+const int DEPTH_MAP_HEIGHT = 1024;
+
+// light setting
+glm::vec3 lightPosition = glm::vec3(8.0f, 4.0f, 5.0f);
+glm::vec3 lightColor = glm::vec3(1.0f, 1.0f, 1.0f);
+float lightNearPlane = 0.1f;
+float lightFarPlane = 20.0f;
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow *window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "OpenGL_SSAO", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glEnable(GL_DEPTH_TEST);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // calculate the passed time from last frame
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        processInput(window);
+
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+void mouse_callback(GLFWwindow *window, double xPos, double yPos)
+{
+    if (firstMouse)
+    {
+        lastX = xPos;
+        lastY = yPos;
+        firstMouse = false;
+    }
+
+    float xoffset = xPos - lastX;
+    float yoffset = lastY - yPos;
+
+    lastX = xPos;
+    lastY = yPos;
+
+    mainCamera.ProcessMouseMovement(xoffset, yoffset);
+}
+void scroll_callback(GLFWwindow *window, double xOffset, double yOffset)
+{
+    mainCamera.ProcessMouseScroll(yOffset);
+}
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        mainCamera.ProcessKeyboard(FORWARD, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        mainCamera.ProcessKeyboard(BACKWARD, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        mainCamera.ProcessKeyboard(LEFT, deltaTime);
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        mainCamera.ProcessKeyboard(RIGHT, deltaTime);
+    }
+}
